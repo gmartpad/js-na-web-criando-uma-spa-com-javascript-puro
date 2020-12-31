6ec7332429ef443f7a66db5ddc0a755ec8e29de5
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-12-30 21:20:03 -0300

    Refatorando a listagem

--- petshop/src/componentes/cadastro/componente-cadastro.js
+++ petshop/src/componentes/cadastro/componente-cadastro.js
@@ -2,30 +2,26 @@ import { cadastrarClientes } from "../../api/cliente";
 
 const { default: eventoEnvia } = require("./cadastro-clientes");
 
-const form = document.createElement('form');
-
-const cadastro = `
-    <div class="container">
-      <div class="form-group">
-        <label>CPF</label>
-        <input type="number" class="form-control" data-cpf placeholder="Digite seu CPF aqui" />
-      </div>
-      <div class="form-group">
-        <label>Nome</label>
-        <input type="text" class="form-control" data-nome placeholder="Digite seu nome aqui" />
-      </div>
-      <button type="submit" class="btn btn-primary">Enviar</button>
-    </div>`;
-
-form.innerHTML = cadastro;
-
 const inicializaCadastro = () => {
-  const container = document.querySelector('[data-container]');
-  eventoEnvia(form);
-  container.innerHTML = ``;
-  container.appendChild(form);
-  return form;
+  const form = document.createElement('form');
 
+  const cadastro = `
+      <div class="container">
+        <div class="form-group">
+          <label>CPF</label>
+          <input type="number" class="form-control" data-cpf placeholder="Digite seu CPF aqui" />
+        </div>
+        <div class="form-group">
+          <label>Nome</label>
+          <input type="text" class="form-control" data-nome placeholder="Digite seu nome aqui" />
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </div>`;
+
+  form.innerHTML = cadastro;
+
+  eventoEnvia(form);
+  return form;
 }
 
 export default inicializaCadastro;
