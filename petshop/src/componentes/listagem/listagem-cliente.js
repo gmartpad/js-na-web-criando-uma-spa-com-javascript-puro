5ac5afe32982125f468dbc67802270adcd75d57b
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-12-30 22:54:02 -0300

    Refatorando a listagem

--- petshop/src/componentes/listagem/listagem-cliente.js
+++ petshop/src/componentes/listagem/listagem-cliente.js
@@ -1,6 +1,5 @@
 import { deletaCliente, listarClientes} from '../../api/cliente.js'
 import "../../assets/css/clientes.css";
-import inicializaCadastro from '../cadastro/componente-cadastro';
 
 const removeCliente = (id) => {
   if(confirm("Deseja deletar o cliente ?")){
@@ -9,38 +8,11 @@ const removeCliente = (id) => {
   }
 }
 
-const conteudo = `
+const criaCorpoTabela = ( tabela ) => {
 
-  <thead class="thead-dark">
-    <tr>
-      <th scope="col">CPF</th>
-      <th scope="col">Nome</th>
-      <th scope="col"></th>
-      <th scope="col">
-        <a class="btn btn-primary">Novo Cliente</a>
-      </th>
-    </tr>
-  </thead>
+  const corpoTabela = document.createElement('tbody');
 
-`
-
-const container = document.querySelector('[data-container]');
-const tabela = document.createElement('table');
-
-tabela.innerHTML = conteudo;
-tabela.classList.add('table');
-
-container.appendChild(tabela);
-
-const novoCliente = document.querySelector('.btn');
-
-novoCliente.addEventListener('click', ()=>{
-  inicializaCadastro();
-})
-
-const corpoTabela = document.createElement('tbody');
-
-const exibeCliente = (cpf, nome, id) => {
+  const exibeCliente = (cpf, nome, id) => {
     const linha = document.createElement('tr');
 
     const conteudoLinha = `
@@ -49,23 +21,47 @@ const exibeCliente = (cpf, nome, id) => {
     <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
     <a href="./componentes/edita/edita-clientes.html?id=${id}">
     <button type=""button class="btn btn-info">Editar</button>
-    </a>
-    
-    
-`
+    </a>       
+    `
   
     linha.innerHTML = conteudoLinha;
     return linha;
   };
-  
+
   listarClientes().then( exibe => {
-  exibe.forEach(indice => {
-    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
+    exibe.forEach(indice => {
+      corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id))
+    })
   })
- }
+  tabela.appendChild(corpoTabela);
 
- )
-tabela.appendChild(corpoTabela);
-  
- 
+}
 
+const inicializaTabela = () => {
+  const cabecalho = `
+
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">CPF</th>
+      <th scope="col">Nome</th>
+      <th scope="col"></th>
+      <th scope="col">
+        <a class="btn btn-primary" onclick="navegacao('/cadastro'); return false;">Novo Cliente</a>
+      </th>
+    </tr>
+  </thead>
+
+`
+
+const tabela = document.createElement('table');
+
+tabela.innerHTML = cabecalho;
+tabela.classList.add('table');
+
+criaCorpoTabela(tabela);
+
+return tabela;
+
+}
+
+export default inicializaTabela;
